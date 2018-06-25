659721278e4b9dfbe5c86545df4e5b8328cc8846
XUJINKAI <XUJINKAI@users.noreply.github.com>
2018-06-26 02:39:41 +0800

    ok

--- WpfService/Controller.cs
+++ WpfService/Controller.cs
@@ -7,21 +7,49 @@ using System.Text;
 using System.Threading.Tasks;
 using WpfService.Model;
 using XJK;
+using XJK.AOP;
+using XJK.AOP.SocketRpc;
+using XJK.Network;
+using XJK.Network.Socket;
 using XJK.Serializers;
 using XJK.SysX;
 
 namespace WpfService
 {
+    public class RpcServerProxy : SocketRpcServer
+    {
+        public int Port { get; private set; }
+
+        public RpcServerProxy(int port)
+        {
+            Port = port;
+            Serve("127.0.0.1", port);
+        }
+
+        public override void DispatchInvoke(Action action)
+        {
+            App.Current.Dispatcher.Invoke(action);
+        }
+
+        protected override object GetExcuteObject()
+        {
+            return Controller.Instance;
+        }
+    }
+
     public class Controller : IService
     {
         public static IClient ClientProxy { get; private set; }
-
+        public static RpcServerProxy RpcServerProxy { get; private set; }
 
         public static Controller Instance { get; private set; }
 
         static Controller()
         {
             Instance = new Controller();
+            int Port = NetHelper.GetAvailablePort(9000);
+            RpcServerProxy = new RpcServerProxy(Port);
+            ClientProxy = MethodProxy.CreateProxy<IClient>(RpcServerProxy);
         }
 
         private Controller() { }
@@ -29,47 +57,66 @@ namespace WpfService
         #region IService
         public Task<bool> ClearClipboardList()
         {
-            throw new NotImplementedException();
+            Clipboards.Clear();
+            return Task.FromResult(true);
         }
 
         public Task<ClipboardWrapper> GetClipboardWrapper()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(Clipboards.Instance.ClipboardWrapper);
         }
 
         public Task<Setting> GetSetting()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(new Setting());
         }
 
         public Task SetClipboard(ClipboardItem clipboardItem)
         {
-            throw new NotImplementedException();
+            RpcServerProxy.DispatchInvoke(() =>
+            {
+                Clipboards.SetClipboard(clipboardItem);
+            });
+            return Task.CompletedTask;
         }
 
         public Task SetClipboard(List<ClipboardItem> list)
         {
-            throw new NotImplementedException();
+            RpcServerProxy.DispatchInvoke(() =>
+            {
+                Clipboards.SetClipboard(list);
+            });
+            return Task.CompletedTask;
         }
 
         public Task<bool> SetSetting(Setting setting)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public Task ShowUwpWindow()
         {
-            throw new NotImplementedException();
+            App.Current.MainWindow.Show();
+            return Task.CompletedTask;
         }
 
         public Task Shutdown()
         {
-            throw new NotImplementedException();
+            RpcServerProxy.DispatchInvoke(() =>
+            {
+                App.Current.Shutdown();
+            });
+            return Task.CompletedTask;
         }
 
         public Task WriteDataFile()
         {
-            throw new NotImplementedException();
+            RpcServerProxy.DispatchInvoke(() =>
+            {
+                WriteClipboardsFile();
+                WriteSettingFile();
+            });
+            return Task.CompletedTask;
         }
         #endregion
 
